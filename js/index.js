c62f5b993b6bef78c7921cbefd259657e96b3fa4
CarlosDev <136222293+carlosdevuser@users.noreply.github.com>
2023-07-10 15:18:18 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// code here
+const btnMenu = document.querySelector('.open-menu')
+const btnIcon = document.querySelector('#menu-open-icon')
+const navbar = document.querySelector('.navbar')
+
+btnMenu.addEventListener('click', () => {
+	navbar.classList.toggle('active')
+	btnIcon.classList.toggle('fa-bars')
+	btnIcon.classList.toggle('fa-close')
+})
